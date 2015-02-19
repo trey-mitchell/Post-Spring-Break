fd6e4c1e752881a33ef164c206df1c051666f861
trey-mitchell <tkmdog@gmail.com>
2015-02-19 10:07:56 -0600

    Try-catch update for Driver.

--- LinkedList/src/Driver.java
+++ LinkedList/src/Driver.java
@@ -3,18 +3,21 @@ public class Driver {
 
 	static public void main(String[] args) throws Exception
 	{
-		//this removes the 25.
 		LinkedList ll = new LinkedList();
 		ll.addFront(21);
 		ll.addFront(22);
 		ll.addFront(25);
 		ll.addFront(24);
 		ll.addFront(21);
-		ll.display();
-		ll.removeAtIndex(2);
-		ll.display();
 		//ll.display();
-		//ll.removeAtIndex(5);
+		try
+		{
+			ll.removeAtIndex(-1);
+		}
+		catch(Exception e)
+		{
+			System.out.println("You did something wrong, try again with a different index.");
+		}
 		//ll.display();
 	}
 }
